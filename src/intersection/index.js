5ebbafe975489fb6d4ddd65e5d426444aae82106
Samantha Brown <113550193+Samantha-Source@users.noreply.github.com>
2023-01-21 16:06:38 -0500

    array intersection algorithm

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,18 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+    let map = new Map();
+    let set = new Set();
+
+    a.forEach((element) => map.set(element));
+
+    b.forEach((element) => {
+        if (map.has(element)){
+            set.add(element)
+        }
+    })
+
+    let output = [...set];
+    return output;
+}
 
 module.exports = intersection;
+
