d96dcf4b465825a499836a724c2b5406220a51e5
Samantha Brown <113550193+Samantha-Source@users.noreply.github.com>
2023-01-21 16:41:21 -0500

    Final
    all tests passing

--- src/sameLetters/index.js
+++ src/sameLetters/index.js
@@ -2,6 +2,27 @@
  * return a Map of the unique letters and arrays of words that are made up of those letters
  * @param {array} words An array  of words
  */
-function sameLetters(words) {}
+function sameLetters(words) {
+    let results = new Map();
+    words.forEach((word) => {
+        word.toLowerCase();
+        const noDupes = new Set(word);
+        const noDupesArray = [...noDupes];
+        noDupesArray.sort();
+        let joined = noDupesArray.join("");
+
+        if (results.get(joined)){
+            results.get(joined).push(word)
+        }
+        else {
+            results.set(joined, [word])
+        }
+    })
+    return results;
+}
 
 module.exports = sameLetters;
+
+let words = ["pair", "per", "pole", "pear", "peer", "reap", "lope"]
+
+console.log(sameLetters(words))
--- src/sumPairs/index.js
+++ src/sumPairs/index.js
@@ -3,6 +3,35 @@
  * @param {Array} A an array of numbers
  * @param {Number} N an integer value
  */
-function sumPairs(A, N) {}
+function sumPairs(A, N) {
+    let numbers = new Map();
+    let solution = new Map();
+
+    A.forEach((e) => numbers.set(e));
+
+
+    A.forEach((e) => {
+        let diff = N - e;
+        if (diff !== e){
+            if(numbers.has(diff) && e < diff){
+                solution.set([e, diff])
+            }
+            if(numbers.has(diff) && diff < e){
+                null;
+            }
+        }
+    })
+
+    let output = [];
+    for (let key of solution.keys()){
+        output.push(key)
+    }
+
+    return output;
+}
 
 module.exports = sumPairs;
+
+let A = [1, 2, 3, 4]
+let N = 7
+console.log(sumPairs(A, N))
